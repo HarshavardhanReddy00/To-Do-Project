0e9b20a660552f51f21d99fb0280d0a4b346b9e5
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-03-28 14:24:03 +0530

    added methods to perform operations

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import Header from './components/Header'
+import ToDoList from './components/ToDoList'
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (text) => {
+    const newTodo = { id: Date.now(), text, completed: false }
+    setTodos([...todos, newTodo])
+  }
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    )
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  const editTodo = (id, newText) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, text: newText } : todo
+      )
+    )
+  }
+
+  return (
+    <div className="app">
+      <Header addTodo={addTodo} />
+      <ToDoList
+        todos={todos}
+        toggleComplete={toggleComplete}
+        deleteTodo={deleteTodo}
+        editTodo={editTodo}
+      />
+    </div>
+  )
+}
+
+export default App
